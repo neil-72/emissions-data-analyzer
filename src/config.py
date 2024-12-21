d4bcae35ddb1879520a2901edd1189d8ecfd8535
neil-72 <andersonneil36@gmail.com>
2024-12-21 10:28:34 +0000

    Update config.py

--- src/config.py
+++ src/config.py
@@ -27,9 +27,12 @@ MAX_PDF_SIZE = 50 * 1024 * 1024  # 50MB
 PDF_TIMEOUT = 30  # seconds
 
 # Search settings
-SEARCH_YEARS = [datetime.now().year, datetime.now().year - 1]  # Current and previous year
+SEARCH_YEARS = [
+    datetime.now().year,     # 2024
+    datetime.now().year - 1, # 2023
+    datetime.now().year - 2  # 2022 - some companies' latest is still 2022
 MAX_RESULTS_PER_SEARCH = 5
-MAX_REPORT_AGE_YEARS = 2
+MAX_REPORT_AGE_YEARS = 4
 
 # Unit conversion settings
 DEFAULT_UNIT = 'metric tons CO2e'
